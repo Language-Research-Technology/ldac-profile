80c87d8f1aab5e69f847c7a004d14b6e527d63e6
Peter Sefton <pt@ptsefton.com>
2022-12-06 16:23:44 +1100

    Added validation for frictionless schema

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -1,11 +1,10 @@
-const {ROCrate} = require('ro-crate');
-const {LdacProfile} = require('../index.js');
+const { ROCrate } = require('ro-crate');
+const { LdacProfile } = require('../index.js');
 const rules = require('../lib/rules');
 const constants = require('../lib/constants');
 const assert = require('assert');
 const fs = require('fs');
-
-const opt = {alwaysAsArray: true, link: true};
+const opt = { alwaysAsArray: true, link: true };
 
 function hasClause(results, rule, id) {
   if (id) {
@@ -14,16 +13,22 @@ function hasClause(results, rule, id) {
   return results.some((r) => r.clause === rule.clause);
 }
 
-describe("Dataset", function () {
+function hasMessage(results, message, id) {
+  if (id) {
+    return results.some((r) => r.message === message && r.entity === id);
+  }
+  return results.some((r) => r.message === message);
+}
 
-  it("must have type", function () {
+describe('Dataset', function () {
+  it('must have type', function () {
     const crate = new ROCrate({}, opt);
     const result = LdacProfile.validate(crate);
     //const result = LdacProfile.validateEntity(crate.getEntity('adfs'));
     assert(hasClause(result.errors, rules.Dataset['@type']));
   });
 
-  it("must have correct @id", function () {
+  it('must have correct @id', function () {
     const crate = new ROCrate({}, opt);
     crate.rootDataset['@id'] = 'abc';
     const result = LdacProfile.validate(crate);
@@ -31,7 +36,7 @@ describe("Dataset", function () {
     assert(hasClause(result.errors, rules.Common['@id']));
   });
 
-  it("must have name", function () {
+  it('must have name', function () {
     const crate = new ROCrate({}, opt);
     const result = LdacProfile.validate(crate);
     //const result = LdacProfile.validateEntity(crate.getEntity('adfs'));
@@ -39,30 +44,43 @@ describe("Dataset", function () {
   });
 });
 
-describe("RepositoryCollection", function () {
-    it("has conformsTo", function () {
+describe('RepositoryCollection', function () {
+  it('has conformsTo', function () {
     const crate = new ROCrate({}, opt);
     assert(crate.rootDataset);
     crate.rootDataset['@type'] = ['Dataset', 'RepositoryCollection'];
-    crate.rootDataset.conformsTo = {'@id': constants.CollectionProfileUrl};
+    crate.rootDataset.conformsTo = { '@id': constants.CollectionProfileUrl };
     var result = LdacProfile.validate(crate);
-    console.log(result.errors)
+    console.log(result.errors);
     assert(hasClause(result.errors, rules.RepositoryCollection.datePublished));
-
-    crate.rootDataset.datePublished = "2020";
-    crate.rootDataset.name = "2020";
-    crate.rootDataset.description = "SOMETHING";
-    crate.rootDataset.license = {"@id": "http://example.com/license"};
+    crate.rootDataset.datePublished = '2020';
+    crate.rootDataset.name = '2020';
+    crate.rootDataset.description = 'SOMETHING';
+    crate.rootDataset.license = { '@id': 'http://example.com/license' };
     result = LdacProfile.validate(crate);
     console.log(result);
     assert.equal(result.errors.length, 0);
   });
 });
 
+describe('Frictionless schemas', function () {
+  var opt = { alwaysAsArray: true, link: true };
+  it('can check a schema', async function () {
+    this.timeout(50000);
 
-describe("generateSpec", function () {
-  it("can generate spec text", function () {
-    const text = LdacProfile.generateSpec();
-    //console.log(text);
+    const crate = new ROCrate(
+      JSON.parse(fs.readFileSync('examples/art/ro-crate-metadata.json')),
+      opt
+    );
+    var result = LdacProfile.validate(crate);
+    assert(
+      hasMessage(
+        result.info,
+        'DOES have an `conformsTo` property that indicates this is a frictionless data table schema',
+        'ABCNE2.csv'
+      )
+    );
+
+    // No conforms to indicating that the collection conforms to this profile we're validating here
   });
 });
